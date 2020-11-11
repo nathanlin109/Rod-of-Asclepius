bf3af3d97815b5f85cd1fec896e4919c3065a2b0
Ryan Rieger <rmr6894@rit.edu>
2020-11-11 01:25:11 -0500

    Enemies are hidden when not in the light

---
+++ Rod of Asclepius/Assets/Materials/MaskShader.shader
@@ -0,0 +1,11 @@
+﻿Shader "Custom/MaskShader"
+{
+	SubShader
+	{
+		Tags{ "Queue" = "Transparent+1" }
+
+		Pass{
+			Blend Zero One
+		}
+	}
+}
---
+++ Rod of Asclepius/Assets/Scripts/Enemy.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // set render queue to properly mask
+        GetComponent<MeshRenderer>().material.renderQueue = 3002;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
