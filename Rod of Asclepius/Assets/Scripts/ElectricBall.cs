8840309e7b482d3b7ff9d552114ceb63c889b27b
Ryan Rieger <rmr6894@rit.edu>
2020-11-30 01:01:17 -0500

    Added wizard attack

---
+++ Rod of Asclepius/Assets/Scripts/ElectricBall.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElectricBall : MonoBehaviour
+{
+    // Fields
+    public GameObject wizard;
+    private GameObject player;
+    private float destroySelfRange;
+    private bool hasHit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        hasHit = false;
+        destroySelfRange = 80.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // destroy the electric ball when it is far away
+        if (Mathf.Pow(transform.position.x - wizard.transform.position.x, 2) + Mathf.Pow(transform.position.z - wizard.transform.position.z, 2) >= Mathf.Pow(destroySelfRange, 2) || hasHit)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Trigger enter
+    private void OnTriggerEnter(Collider other)
+    {
+        // check for collisions with the player
+        if (hasHit == false)
+        {
+            // damage the player if they are hit
+            if (other.gameObject.tag == "Player")
+            {
+                player = other.gameObject;
+
+                if (player.GetComponent<Player>().health > 0)
+                {
+                    player.GetComponent<Player>().health--;
+                }
+                
+                hasHit = true;
+            }
+        }
+    }
+}
--- Rod of Asclepius/Assets/Scripts/Wizard.cs
+++ Rod of Asclepius/Assets/Scripts/Wizard.cs
@@ -8,10 +8,23 @@ public class Wizard : Enemy
     // Fields
     public GameObject spotLight;
 
+    // Abilities
+    public GameObject electricBallPrefab;
+    public float electricBallCooldown;
+    private float electricBallTimeTillCooldown;
+    public float electricBallRange;
+    public float projectileSpeed;
+
     // Start is called before the first frame update
     protected override void Start()
     {
         base.Start();
+
+        electricBallCooldown = 2.0f;
+        electricBallTimeTillCooldown = 0.0f;
+        electricBallRange = 15f;
+
+        projectileSpeed = 4.0f;
     }
 
     // Update is called once per frame
@@ -19,29 +32,53 @@ public class Wizard : Enemy
     {
         base.Update();
 
+        // check for game state
+        if (sceneMan.GetComponent<SceneMan>().gameState == GameState.Game)
+        {
+            // shoot electric balls when within a certain distance of the player
+            if (Mathf.Pow(player.transform.position.x - transform.position.x, 2) + Mathf.Pow(player.transform.position.z - transform.position.z, 2) <= Mathf.Pow(electricBallRange, 2))
+            {
+                // create the electric ball and reset cooldown
+                if (electricBallTimeTillCooldown >= electricBallCooldown)
+                {
+                    GameObject electricBall = Instantiate(electricBallPrefab, transform.position, Quaternion.identity);
+                    electricBall.GetComponent<Rigidbody>().AddForce(new Vector3(player.transform.position.x - transform.position.x, 0, player.transform.position.z - transform.position.z).normalized * projectileSpeed, ForceMode.Impulse);
+                    electricBallTimeTillCooldown = 0;
+                }
+            }
 
-        // slow down when within a certain distance of the player
-        //if (Mathf.Pow(player.transform.position.x - transform.position.x, 2) + Mathf.Pow(player.transform.position.z - transform.position.z, 2) <= Mathf.Pow(15, 2))
-        //{
-        //    GetComponent<NavMeshAgent>().speed = 3;
-        //}
-        //else
-        //{
-        //    GetComponent<NavMeshAgent>().speed = 6;
-        //}
+            // slow down when within a certain distance of the player
+            if (Mathf.Pow(player.transform.position.x - transform.position.x, 2) + Mathf.Pow(player.transform.position.z - transform.position.z, 2) <= Mathf.Pow(12, 2))
+            {
+                GetComponent<NavMeshAgent>().speed = 4.5f;
+            }
+            // speed up when player is farther away and do not attack
+            else
+            {
+                GetComponent<NavMeshAgent>().speed = 7.0f;
+            }
+
+            // update cooldown
+            electricBallTimeTillCooldown += Time.deltaTime;
+        }
+        // game state is not game
+        else
+        {
+            GetComponent<NavMeshAgent>().speed = 6.0f;
+        }
     }
 
     // Handles collisions w/ player
-    private void OnCollisionStay(Collision collision)
-    {
-        // Handles collision w/ player
-        if (collision.gameObject.tag == "Player" &&
-            player.GetComponent<Player>().hasCollided == false &&
-            player.GetComponent<Player>().health > 0 && silenced == false)
-        {
-            Debug.Log("Damaged Player");
-            player.GetComponent<Player>().health--;
-            player.GetComponent<Player>().hasCollided = true;
-        }
-    }
+    //private void OnCollisionStay(Collision collision)
+    //{
+    //    // Handles collision w/ player
+    //    if (collision.gameObject.tag == "Player" &&
+    //        player.GetComponent<Player>().hasCollided == false &&
+    //        player.GetComponent<Player>().health > 0 && silenced == false)
+    //    {
+    //        Debug.Log("Damaged Player");
+    //        player.GetComponent<Player>().health--;
+    //        player.GetComponent<Player>().hasCollided = true;
+    //    }
+    //}
 }
