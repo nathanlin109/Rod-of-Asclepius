5dc67e3171d2598c2f22d6eb8926f4f081cc3ecd
nathanlin109 <nathanlin109@gmail.com>
2020-12-08 16:30:06 -0500

    cursor changes

--- Rod of Asclepius/Assets/Scripts/SceneMan.cs
+++ Rod of Asclepius/Assets/Scripts/SceneMan.cs
@@ -39,7 +39,7 @@ public class SceneMan : MonoBehaviour
     {
         gameState = GameState.Cutscene1;
         resurrectedMom = false;
-        Cursor.SetCursor(cursorTexture, new Vector2(cursorTexture.width / 2, cursorTexture.height / 2), CursorMode.Auto);
+        Cursor.SetCursor(cursorTexture, new Vector2(cursorTexture.width / 2, cursorTexture.height / 2), CursorMode.ForceSoftware);
         GameObject.Find("AudioManager").GetComponent<AudioMan>().Play("background-sounds");
         playedWinSound = false;
     }
